320edb5ff013c6478cae3de108669d08ac8b5d61
Abbas Haider <abbas_haider12@hotmail.com>
2020-05-08 13:01:02 -0400

    firebase validation

--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,7 @@ module.exports = (env) =>{
             'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
             'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
             'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
-            'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
+            'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID)
 
         })
         ],

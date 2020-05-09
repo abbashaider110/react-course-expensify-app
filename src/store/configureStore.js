11f5fb0749ef122477cdc7cf4182e10ee82aaff1
Abbas Haider <abbas_haider12@hotmail.com>
2020-05-09 16:31:45 -0400

    Final

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,10 @@
-import {createStore, combineReducers,applyMiddleware,comopose} from 'redux'; // combine reducers is used when we are using more than one reducers, read about applyMiddleware
+import {createStore, combineReducers,applyMiddleware,compose} from 'redux'; // combine reducers is used when we are using more than one reducers, read about applyMiddleware
 import thunk from 'redux-thunk';  // thunk lets store disptach function
 import  expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
 import authReducer from '../reducers/auth';
 
-const composeEnhancers =  compose;//window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default () =>{
 // Store Creation
 
